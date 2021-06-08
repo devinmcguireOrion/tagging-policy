e7f3278bc0ed83ce763d6a6ba4a68c804639d660
devinmcguireOrion <devin.mcguire@orion.com>
2021-06-08 09:59:08 -0500

    Added autotag example
    Example is from https://github.com/joeduffy/aws-tags-example. Trying without the proper tags to see if it leaves a warning.

---
+++ test/__main__.py
@@ -0,0 +1,28 @@
+import pulumi
+import pulumi_aws as aws
+from autotag import register_auto_tags
+
+# Automatically inject tags.
+config = pulumi.Config()
+register_auto_tags({
+    'user:Project': pulumi.get_project(),
+    'user:Stack': pulumi.get_stack(),
+    #'user:Cost Center': config.require('costCenter'),
+})
+
+# Create a bunch of AWS resources -- with auto-tags!
+
+bucket = aws.s3.Bucket('my-bucket')
+
+group = aws.ec2.SecurityGroup('web-secgrp',
+    ingress=[
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0']},
+        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0']},
+    ],
+)
+
+server = aws.ec2.Instance('web-server-www',
+    instance_type='t2.micro',
+    ami='ami-0ee02acd56a52998e',
+    vpc_security_group_ids=[ group.id ],
+)
