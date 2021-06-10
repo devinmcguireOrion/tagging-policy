6baaff5b56cafa60bc57ced4442dfbc345462e8c
devinmcguireOrion <devin.mcguire@orion.com>
2021-06-10 17:07:22 -0500

    Fixed error when no tags

--- policy-pack/__main__.py
+++ policy-pack/__main__.py
@@ -7,21 +7,54 @@ from pulumi_policy import (
 )
 from taggable import is_taggable
 
-requiredTags = ["application","backup-by","cost-center","delete-after","environment","product","product-area"]
+requiredTags = ["application", "cost-center", "environment", "product", "product-area"]
+timeTags = ["backup-by","delete-after"] #Should auto-tag check if ec2 or ebs?
+stackTags = ["project", "stack"]
 
-def check_for_tags_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+def has_tags_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
     if is_taggable(args.resource_type):
-        ts = args.props['tags']
-        for rt in requiredTags:
-            if rt not in ts:
-                report_violation(
-                    f"Taggable resource '{args.urn}' is missing required tag '{rt}'")
+        if 'tags' not in args.props:
+            report_violation(
+                f"Taggable resource '{args.urn}' does not have any tags")
+
+def check_for_required_tags_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if is_taggable(args.resource_type):
+        if 'tags' in args.props:
+            ts = args.props['tags']
+            for rt in requiredTags:
+                if rt not in ts:
+                    report_violation(
+                        f"Taggable resource '{args.urn}' is missing required tag '{rt}'")
+
+def check_for_time_tags_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if is_taggable(args.resource_type):
+        if "ebs" in args.resource_type or "ec2" in args.resource_type:
+            if 'tags' in args.props:
+                ts = args.props['tags']
+                for rt in timeTags:
+                    if rt not in ts:
+                        report_violation(
+                            f"Taggable resource '{args.urn}' is missing required tag '{rt}'")
+
+def check_for_stack_tags_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if is_taggable(args.resource_type):
+        if 'tags' in args.props:
+            ts = args.props['tags']
+            for rt in stackTags:
+                if rt not in ts:
+                    report_violation(
+                        f"Taggable resource '{args.urn}' is missing required tag '{rt}'")
 
 check_for_tags = ResourceValidationPolicy(
-    name="check-required-tags",
-    description="Looks for specified tags on each resource.",
+    name="check-for-tags",
+    description="Looks for tags on taggable resources.",
     enforcement_level=EnforcementLevel.ADVISORY,
-    validate=check_for_tags_validator,
+    validate=[
+        has_tags_validator, 
+        check_for_required_tags_validator, 
+        #check_for_stack_tags_validator, 
+        check_for_time_tags_validator,
+    ]
 )
 
 PolicyPack(
